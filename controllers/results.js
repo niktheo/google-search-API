8f37b8f00c6bd0c560bac08e6e6a99d4458a4709
niktheo <nikostheodoridis1999@gmail.com>
2022-07-08 16:28:53 +0300

    Create new router post that takes the info from the DB and deleted the previous test results.

--- controllers/results.js
+++ controllers/results.js
@@ -3,28 +3,18 @@ const express = require('express')
 const router = express.Router()
 const Results = require('../models/results.js')
 // Create POST controller
-router.post('/', (req, res) => {
-  let results = [
-    {
-      title: 'JavaScript Tutorial - W3School',
-      description:
-        'Well organized and easy to understand Web building tutorials with lots of examples of how to use HTML, CSS, JavaScript, SQL, PHP, Python, Bootstrap, Java',
-      url: 'https://www.w3schools.com',
-      links: [
-        {
-          title: 'JavaScript Introduction',
-          url: 'https://www.w3schools.com/js'
-        },
-        {
-          title: 'JS Functions',
-          url: 'https://www.w3schools.com/js'
-        },
-        {
-          title: 'JavaScript Examples',
-          url: 'https://www.w3schools.com/js'
-        }
+router.post('/', async (req, res, next) => {
+  try {
+    let results = await Results.find({
+      // e.description.includes(req.body.searchTerms)
+      $or: [
+        { title: { $regex: req.body.searchTerms, $options: 'i' } },
+        { url: { $regex: req.body.searchTerms, $options: 'i' } },
+        { description: { $regex: req.body.searchTerms, $options: 'i' } },
+        { links: { url: { $regex: req.body.searchTerms, $options: 'i' } } }
+        // { links: [{ title: { $regex: req.body.searchTerms, $options: 'i' } }] }
       ]
-    },
+    })
 
     {
       title: 'JS tutorials',
