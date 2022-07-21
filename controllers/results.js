2fda45bacab74aca0c4678b402774fd20897cde8
niktheo <nikostheodoridis1999@gmail.com>
2022-07-21 18:35:07 +0300

    Update the response to send json format files from database and changed the req.body to req.query to get the info from the input type

--- controllers/results.js
+++ controllers/results.js
@@ -3,27 +3,25 @@ const express = require('express')
 const router = express.Router()
 const Results = require('../models/results.js')
 // Create POST controller
-router.post('/', async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
     let results = await Results.find({
       // e.description.includes(req.body.searchTerms)
       $or: [
-        { title: { $regex: req.body.searchTerms, $options: 'i' } },
-        { url: { $regex: req.body.searchTerms, $options: 'i' } },
-        { description: { $regex: req.body.searchTerms, $options: 'i' } },
-        { 'links.title': { $regex: req.body.searchTerms, $options: 'i' } },
-        { 'links.url': { $regex: req.body.searchTerms, $options: 'i' } }
+        { title: { $regex: req.query.searchTerms || '', $options: 'i' } },
+        { url: { $regex: req.query.searchTerms || '', $options: 'i' } },
+        { description: { $regex: req.query.searchTerms || '', $options: 'i' } },
+        {
+          'links.title': { $regex: req.query.searchTerms || '', $options: 'i' }
+        },
+        { 'links.url': { $regex: req.query.searchTerms || '', $options: 'i' } }
       ]
     })
 
-    res.render('results', { results })
+    res.json(results)
   } catch (err) {}
 
   // res.render('results', { results })
-})
-router.get('/', (req, res) => {
-  let resultsarr = []
-  res.render('results', { resultsarr })
 })
 
 // Export module
